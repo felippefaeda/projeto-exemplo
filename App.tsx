6f1d2779753d502398a812bf2c9c4a047e90b042
Felippe Faêda <felippefaeda@gmail.com>
2022-05-12 15:40:52 -0300

    Alteração na mensagem de boas vindas

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Boa Tarde</Text>
+      <Text>Bom dia</Text>
       <StatusBar style="auto" />
     </View>
   );
